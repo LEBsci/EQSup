c23af66ea510325b8c5d2827838209f61492b36c
LEBsci <80007906+LEBsci@users.noreply.github.com>
2021-03-24 22:22:28 +0100

    Update plantilla1.py
    Comentarios de cada parte de la función

--- Graficadores/plantilla1.py
+++ Graficadores/plantilla1.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-data = os.path.join(THIS_FOLDER, 'bigdata.csv')
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))    #Detecta la ubicacion del archivo
+data = os.path.join(THIS_FOLDER, 'bigdata.csv') #Consigue el archivo de datos para graficar. Ver archivo para comparar el formato
 
 SMALL_SIZE = 12
 MEDIUM_SIZE = 14
@@ -20,14 +20,14 @@ plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 plt.rc('figure', figsize=(8,8))
 
 
-pt = pd.read_csv(data, index_col='electrode')
-miller = pt.index.array.unique().dropna()
+pt = pd.read_csv(data, index_col='electrode')   #La columna 'electrode' se usa para identificar. Puede tener otra informacion
+miller = pt.index.array.unique().dropna()   #Esto hace una lista de cuantos 'electrodos' hay
 
-for n in miller:
-    plt.plot(pt.loc[n].iloc[:,0], pt.loc[n].iloc[:,5], label=n)
-plt.axhline(0, 0, 1, c = 'black')
-plt.xlabel('$E$/V vs Pd/H')
-plt.ylabel(r'$i\,/\,\mathrm{\mu A}$')
-plt.legend()
-plt.savefig('voltagramas.svg')
-plt.show()
+for n in miller:    #Ciclo usando los valores de miller
+    plt.plot(pt.loc[n].iloc[:,0], pt.loc[n].iloc[:,5], label=n) #En el ciclo se grafica cada uno de los 'electrodos' y se guardan
+plt.axhline(0, 0, 1, c = 'black')   #Se grafica el eje cero
+plt.xlabel('$E$/V vs Pd/H') #Nombre de eje X
+plt.ylabel(r'$i\,/\,\mathrm{\mu A}$')   #Nombre de eje Y
+plt.legend()    #Se ubica la leyenda en el mejor lugar automaticamente
+plt.savefig('voltagramas.svg')  #Se guarda la grafica en formato de vectores
+plt.show()  #Se muestra el grafico de forma interactiva
