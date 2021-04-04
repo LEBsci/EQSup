46729ff299338fff47dee71e9ee613a6a86429cb
LEBsci <lebotello@pm.me>
2021-04-04 10:19:42 +0200

    Primera entrada de analizador de isoterma de Frumkin

---
+++ FrumkinAnalysis/frumkin_0.1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+cy = 8
+data = pd.read_csv('Pt322.csv', header=None, skiprows=1)
+cvraw = data.iloc[:,cy:cy+2].dropna()
+cv = cvraw.sort_values(by=[cy])
+# plt.scatter(cv.iloc[:,0], cv.iloc[:,1])
+# plt.show()
+print(cv)
+# cv.to_csv('out.csv')
+'''
+version 0.1
+ready to plot one cv
+'''
